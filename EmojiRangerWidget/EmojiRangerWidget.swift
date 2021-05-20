e5be360373152d2502bdc74c9e9a70ef368833b9
Elena Margolin <emargolin101@gmail.com>
2021-05-20 15:44:07 -0700

    new configurations but only displays placeholder

--- EmojiRangerWidget/EmojiRangerWidget.swift
+++ EmojiRangerWidget/EmojiRangerWidget.swift
@@ -8,22 +8,36 @@ A widget that shows the avatar for a single character.
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
     
     public typealias Entry = SimpleEntry
     
-    func placeholder(in context: Context) -> SimpleEntry {
-        return SimpleEntry(date: Date(), character: .panda)
+    func character(for configuration:
+        CharacterSelectionIntent) -> CharacterDetail {
+        switch configuration.hero {
+        case .panda:
+            return .panda
+        case .egghead:
+            return .egghead
+        case .spouty:
+            return .spouty
+        default:
+            return .panda
+        }
     }
     
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
-        let entry = SimpleEntry(date: Date(), character: .panda)
+    func placeholder(in context: Context) -> SimpleEntry {
+        return SimpleEntry(date: Date(), character: .panda, relevance: nil) // added relevance, not in tutorial
+    }
+    
+    func getSnapshot(for configuration: CharacterSelectionIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), character: .panda, relevance: nil)
         completion(entry)
     }
     
     // Changed this function to update at certain times
-    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) ->()) {
-        let selectedCharacter = CharacterDetail.panda
+    func getTimeline(for configuration: CharacterSelectionIntent, in context: Context, completion: @escaping (Timeline<SimpleEntry>) ->()) {
+        let selectedCharacter = character(for: configuration)
         let endDate = selectedCharacter.fullHealthDate
         let oneMinute: TimeInterval = 60
         var currentDate = Date()
@@ -41,13 +55,13 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     public let date: Date
-    let character: CharacterDetail
+    let character: CharacterDetail // <variableName>: <type>
     let relevance: TimelineEntryRelevance?
 }
 
 struct PlaceholderView: View {
     var body: some View {
-        EmojiRangerWidgetEntryView(entry: SimpleEntry(date: Date(), character: .panda))
+        EmojiRangerWidgetEntryView(entry: SimpleEntry(date: Date(), character: .panda, relevance: nil))
 //            .isPlaceholder(true) // getting errors for this...
     }
 }
@@ -89,11 +103,11 @@ struct EmojiRangerWidget: Widget {
     private let kind: String = "EmojiRangerWidget"
 
     public var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, provider: Provider()) { entry in
+        IntentConfiguration(kind: kind, intent: CharacterSelectionIntent.self, provider: Provider()) { entry in
             EmojiRangerWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("Ranger Detail")
-        .description("See your favorite ranger.")
+        .description("See your favorite ranger!!!")
         .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
@@ -101,7 +115,7 @@ struct EmojiRangerWidget: Widget {
 struct Widget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            EmojiRangerWidgetEntryView(entry: SimpleEntry(date: Date(), character: .panda))
+            EmojiRangerWidgetEntryView(entry: SimpleEntry(date: Date(), character: .panda, relevance: nil))
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
             
             PlaceholderView()
